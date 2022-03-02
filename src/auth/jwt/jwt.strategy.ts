0ebcb366bfb83636529d0300f16396e7405098fb
Ko-GyeongTae <kokt0203@naver.com>
2022-03-02 23:12:48 +0900

    feat(jwt): add jwt strategy

---
+++ src/auth/jwt/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+
+@Injectable()
+export class JWTStrategy extends PassportStrategy(Strategy) {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: process.env.JWT_SECRET
+        });
+    }
+
+    async validate(payload) {
+        const account = await this.prismaService.account.findUnique({ 
+            where: { 
+              id: payload.id 
+            }
+          })
+          return account;
+    }
+}
